3535d923e09b111b6a19e71f9a7b72bddfec30f0
sshindanai <sshindanai.m@gmail.com>
2021-03-01 00:02:05 +0700

    1st version real-time chatroom

---
+++ client.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from peer
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer
+	maxMessageSize = 512
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+type connection struct {
+	ws   *websocket.Conn
+	send chan []byte
+}
+
+// upgrade connection to support Web Socket
+func serveWs(w http.ResponseWriter, r *http.Request, roomId string) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+	c := &connection{send: make(chan []byte, 256), ws: ws}
+	s := subscription{c, roomId}
+	h.register <- s
+	go s.writePump()
+	go s.readPump()
+
+}
+
+// Send message from socket to hub
+func (s subscription) readPump() {
+	c := s.conn
+	defer func() {
+		h.unregister <- s
+		c.ws.Close()
+	}()
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(string) error {
+		c.ws.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+	for {
+		_, msg, err := c.ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err,
+				websocket.CloseGoingAway) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		m := message{msg, s.room}
+		h.broadcast <- m
+	}
+}
+
+// Send message from hub to socket
+func (s *subscription) writePump() {
+	c := s.conn
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+// StartServer for application
+var (
+	router *gin.Engine
+)
+
+func main() {
+	go h.run()
+	router = gin.New()
+	router.LoadHTMLFiles("index.html")
+
+	router.GET("/room/:roomId", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	router.GET("/ws/:roomId", func(c *gin.Context) {
+		roomId := c.Param("roomId")
+		serveWs(c.Writer, c.Request, roomId)
+	})
+
+	router.Run("0.0.0.0:8080")
+}
